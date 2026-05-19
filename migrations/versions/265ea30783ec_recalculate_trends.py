8826878d18550f5db78768e7f1851404325dd42c
leute <leute@archlinux>
2026-05-19 17:09:15 +0300

    Change incoming migration to follow correct latest migration

--- migrations/versions/265ea30783ec_recalculate_trends.py
+++ migrations/versions/265ea30783ec_recalculate_trends.py
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "265ea30783ec"
-down_revision: Union[str, Sequence[str], None] = "3112835c5adc"
+down_revision: Union[str, Sequence[str], None] = "c4a7d8e9f012"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
