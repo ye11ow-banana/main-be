7c07bee1851ad081da46d1f89400bba0d4866cc3
leute <leute@archlinux>
2026-06-02 17:52:26 +0300

    Remove unnecessary alembic merge migration

--- migrations/versions/af99dc1e7da9_add_friendships_table.py
+++ migrations/versions/af99dc1e7da9_add_friendships_table.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "af99dc1e7da9"
-down_revision: Union[str, Sequence[str], None] = "3112835c5adc"
+down_revision: Union[str, Sequence[str], None] = "d8d3b50de011"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
